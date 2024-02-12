d35fcee8e82b7c6a1efd6ad45715a317112ef8f5
Ejiajshd <159745012+Ejiajshd@users.noreply.github.com>
2024-02-11 22:26:42 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using LinkvertiseBypass.Linkvertise;
 
 Linkvertise linkvertise = new Linkvertise();
-await linkvertise.Bypass(new Uri("linkvertise_url_here"));
+await linkvertise.Bypass(new Uri("https://linkvertise.com/514008/158.69441627856685/dynamic?s=0&r=aHR0cHM6Ly9nYXRld2F5LnBsYXRvYm9vc3QuY29tL2EvMjU2OT9pZD01NTU5NDIwOTg3JnRrPWpiOGI%3D&o=sharing"));
